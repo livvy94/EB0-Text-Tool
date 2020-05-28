a93390b1c6a18bef049e18ea3c3779540a134800
vince94 <haxxor199x@gmail.com>
2020-05-28 10:05:23 -0400

    Add file I/O

---
+++ FileIO.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+
+namespace EB0_Text_Tool
+{
+    class FileIO
+    {
+        internal static string FileExists(string romPath)
+        {
+            //Check as a full path
+            if (File.Exists(romPath))
+                return romPath;
+
+            //Check as [Current Directory]/romPath
+            var longPath = Path.Combine(Directory.GetCurrentDirectory(), romPath);
+            if (File.Exists(longPath))
+                return longPath;
+
+            return "ERROR! Couldn't find either " + romPath + " or " + longPath;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace EB0_Text_Tool
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var romPath = string.Empty;
+            Console.Title = "EarthBound Zero Text Tool";
+
+            if (args is null)
+            {
+                //If nothing comes up, ask for a filename
+                Console.WriteLine("Please type the filename of the ROM you want to open.");
+                Console.WriteLine("(For future reference, you can drag and drop it or use command line arguments!)");
+
+                romPath = Console.ReadLine();
+            }
+
+            romPath = FileIO.FileExists(romPath);
+
+            if (romPath.StartsWith("ERROR")) //existence check
+            {
+                Console.WriteLine(romPath);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine("Dump text or insert text? d/i");
+            var input = Console.ReadLine();
+
+            if (input.ToLower().StartsWith("d"))
+            {
+                Console.WriteLine("Dumping text...");
+            }
+            else //just hitting enter will insert, since that's probably what people'll be doing 99% of the time they use this tool
+            {
+                Console.WriteLine("Inserting text...");
+            }
+
         }
     }
 }
